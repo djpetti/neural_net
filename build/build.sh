567e18da7d318c77fe1d03799e18b875b5c91ad1
Daniel Petti <djpetti@gmail.com>
2014-05-17 08:33:00 -0700

    Add gyp and ninja to downloaded externals.
    I was going to add openmpi as well, but it's such a big package
    that I favored using the package instead.

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 ./download_externals.sh
-GYP_GENERATORS=ninja gyp neural_net.gyp -I neural_net.gypi \
+GYP=externals/downloaded/gyp/gyp
+NINJA=externals/downloaded/ninja/ninja
+
+GYP_GENERATORS=ninja ${GYP} neural_net.gyp -I neural_net.gypi \
     --toplevel-dir=`pwd` --depth=.. --no-circular-check
-ninja -C out/Default/ all
+${NINJA} -C out/Default/ all
--- build/download_externals.sh
+++ build/download_externals.sh
@@ -4,6 +4,24 @@ cd externals
 mkdir -p downloaded
 cd downloaded
 
+# Gyp
+if [ ! -d "gyp" ]
+then
+  svn checkout http://gyp.googlecode.com/svn/trunk/ gyp
+fi
+
+# Ninja
+if [ ! -d "ninja" ]
+then
+  git clone https://github.com/martine/ninja.git
+fi
+if [ ! -f "ninja/ninja" ]
+then
+  cd ninja
+  ./bootstrap.py
+  cd ..
+fi
+
 # Gtest
 GTEST_VERSION=1.7.0
 GTEST_NAME=gtest-${GTEST_VERSION}
